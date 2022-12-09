9c997252180a473bce3af782a9a525268038720c
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 02:02:25 -0600

    Feat: toggle card on click

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import { Box, Image } from '@chakra-ui/react';
 
+import { COVER_IMAGE } from '@utils/constants';
+
 function Card({ url, name }) {
+  const [imageSRC, setImageSRC] = useState(url);
+
+  const onHandleClick = () => {
+    setImageSRC((prevImage) => (prevImage === url ? COVER_IMAGE : url));
+  };
+
   return (
     <Box m={6} h={40} w={32}>
-      <Image alt={name} cursor="pointer" objectFit="cover" src={url} />
+      {imageSRC === url ? (
+        <Image
+          alt={name}
+          cursor="pointer"
+          objectFit="cover"
+          src={url}
+          onClick={onHandleClick}
+        />
+      ) : (
+        <Image
+          alt={name}
+          cursor="pointer"
+          objectFit="cover"
+          src={COVER_IMAGE}
+          onClick={onHandleClick}
+        />
+      )}
     </Box>
   );
 }
