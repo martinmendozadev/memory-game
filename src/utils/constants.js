5a95ca6af7c12e1b882168de965ef426eaba527b
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 00:42:44 -0600

    Add: utils folder

---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line import/prefer-default-export
+export const IMAGES_URLS = [
+  {
+    name: 'cover',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565809/memory-game/cover_ocjwyl.png',
+  },
+  {
+    name: 'image1',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565809/memory-game/009_aqwpot.png',
+  },
+  {
+    name: 'image2',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565809/memory-game/005_izm9ef.png',
+  },
+  {
+    name: 'image3',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565808/memory-game/008_cp21nb.png',
+  },
+  {
+    name: 'image4',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565809/memory-game/007_nuvbz3.png',
+  },
+  {
+    name: 'image5',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565809/memory-game/004_emur9l.png',
+  },
+  {
+    name: 'image6',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565808/memory-game/002_jtx3up.png',
+  },
+  {
+    name: 'image7',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565808/memory-game/006_izuufd.png',
+  },
+  {
+    name: 'image8',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565808/memory-game/001_yspzaa.png',
+  },
+  {
+    name: 'image9',
+    url: 'https://res.cloudinary.com/dtezbna78/image/upload/v1670565808/memory-game/003_juo3wp.png',
+  },
+];
