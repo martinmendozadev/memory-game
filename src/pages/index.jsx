addde3023de3e9b5dd18943b137ec4f23f6fc9bd
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 01:22:38 -0600

    Add: restart button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,21 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
 
 import Card from '@components/Card';
 import { IMAGES_URLS } from '@utils/constants';
 
 export default function Home() {
   return (
-    <Flex justify="space-evenly" wrap="wrap">
-      {IMAGES_URLS?.map((image) => (
-        <Card key={`${image.name}`} url={image.url} name={image.name} />
-      ))}
+    <Flex align="center" direction="column">
+      <Flex justify="space-evenly" wrap="wrap">
+        {IMAGES_URLS?.map((image) => (
+          <Card key={`${image.name}`} url={image.url} name={image.name} />
+        ))}
+      </Flex>
+      <Box>
+        <Button colorScheme="teal" mt={6} size="md">
+          Restart
+        </Button>
+      </Box>
     </Flex>
   );
 }
