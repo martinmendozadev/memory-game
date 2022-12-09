858c81655e1ec02de5aa815bb9196eaca9ccbcfc
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 01:21:46 -0600

    Update: card image styles

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,7 @@ import { Box, Image } from '@chakra-ui/react';
 function Card({ url, name }) {
   return (
     <Box m={6} h={40} w={32}>
-      <Image objectFit="cover" src={url} alt={name} />
+      <Image alt={name} cursor="pointer" objectFit="cover" src={url} />
     </Box>
   );
 }
