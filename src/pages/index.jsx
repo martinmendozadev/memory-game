c577ff93930d29b97b984e851804419233a6abe2
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 23:53:22 -0600

    Add: card parner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Box, Button, Flex } from '@chakra-ui/react';
 
 import Card from '@components/Card';
 import { IMAGES_URLS } from '@utils/constants';
 
 export default function Home() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const allFruits = [...IMAGES_URLS, ...IMAGES_URLS]
+      .sort(() => Math.random() - 0.5)
+      .map((item) => ({ ...item, id: Math.random() }));
+
+    setCards(allFruits);
+  }, []);
+
   return (
     <Flex align="center" direction="column">
       <Flex justify="space-evenly" wrap="wrap">
-        {IMAGES_URLS?.map((image) => (
-          <Card key={`${image.name}`} url={image.url} name={image.name} />
+        {cards?.map((card) => (
+          <Card
+            key={`${card.name}`}
+            url={card.url}
+            name={card.name}
+            flipped={false}
+          />
         ))}
       </Flex>
       <Box>
