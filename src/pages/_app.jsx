d742a1c2d2c26e6827309b0d96d2cb2d2c769538
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-10 13:16:47 -0600

    Add: react-icons

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,13 +2,16 @@ import { appWithTranslation } from 'next-i18next';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import '@styles/globals.css';
+import BaseLayout from '@components/_layouts/BaseLayout';
 import { AppContextProvider } from '@context/appContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <AppContextProvider>
-        <Component {...pageProps} />
+        <BaseLayout>
+          <Component {...pageProps} />
+        </BaseLayout>
       </AppContextProvider>
     </ChakraProvider>
   );
