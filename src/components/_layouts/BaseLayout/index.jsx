f6bf5eb62499492645f63952eff4a609cb7def05
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-10 14:03:27 -0600

    Fix: container height

--- src/components/_layouts/BaseLayout/index.jsx
+++ src/components/_layouts/BaseLayout/index.jsx
@@ -5,13 +5,13 @@ import Footer from '@components/Footer';
 
 function BaseLayout({ children }) {
   return (
-    <>
+    <Flex direction="column" justify="space-between" h="100vh">
       <Header />
       <Flex justify="center" align="center" my={6}>
         {children}
       </Flex>
       <Footer />
-    </>
+    </Flex>
   );
 }
 
