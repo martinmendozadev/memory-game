6fdf81603c5ab8f7a760d093a7bd4ec41eb8cfbf
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-10 13:46:19 -0600

    Feat: change lenguage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,34 @@
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-import { Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 export default function Header() {
+  const { locale } = useRouter();
   const { t } = useTranslation('common');
 
   return (
     <Flex justify="center" bg={useColorModeValue('gray.50', 'gray.900')} py={4}>
-      <Heading>{t('header.title')}</Heading>
+      <Flex justify="space-evenly" w="100%">
+        <Box>
+          <Heading>{t('header.title')}</Heading>
+        </Box>
+        <Box>
+          <Link as={NextLink} href="/" locale={locale === 'en' ? 'es' : 'en'}>
+            <Button variant="outline">
+              <Text casing="capitalize">{locale}</Text>
+            </Button>
+          </Link>
+        </Box>
+      </Flex>
     </Flex>
   );
 }
