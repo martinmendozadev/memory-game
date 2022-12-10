7c0d460263c43cbf179b86fcc25cdc71472f57cc
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-10 14:23:43 -0600

    feat: change theme action

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,14 +6,20 @@ import {
   Button,
   Flex,
   Heading,
+  IconButton,
   Link,
   Text,
+  Tooltip,
+  useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { FaRegMoon, FaSun } from 'react-icons/fa';
 
 export default function Header() {
   const { locale } = useRouter();
   const { t } = useTranslation('common');
+  const { colorMode, toggleColorMode } = useColorMode();
+  const local = locale === 'en' ? 'es' : 'en';
 
   return (
     <Flex justify="center" bg={useColorModeValue('gray.50', 'gray.900')} py={4}>
@@ -21,13 +27,30 @@ export default function Header() {
         <Box>
           <Heading>{t('header.title')}</Heading>
         </Box>
-        <Box>
-          <Link as={NextLink} href="/" locale={locale === 'en' ? 'es' : 'en'}>
-            <Button variant="outline">
-              <Text casing="capitalize">{locale}</Text>
-            </Button>
+        <Flex>
+          <Link as={NextLink} href="/" locale={local} mr={4}>
+            <Tooltip
+              label={t('buttons.changeLanguageTooltip')}
+              aria-label={t('buttons.tooltip')}>
+              <Button variant="outline">
+                <Text casing="capitalize">{local}</Text>
+              </Button>
+            </Tooltip>
           </Link>
-        </Box>
+
+          <Tooltip
+            label={t('buttons.themeTooltip')}
+            aria-label={t('buttons.tooltip')}>
+            <IconButton
+              aria-label={t('buttons.theme')}
+              fontSize="sm"
+              fontWeight={600}
+              icon={colorMode === 'dark' ? <FaSun /> : <FaRegMoon />}
+              onClick={toggleColorMode}
+              variant="outline"
+            />
+          </Tooltip>
+        </Flex>
       </Flex>
     </Flex>
   );
