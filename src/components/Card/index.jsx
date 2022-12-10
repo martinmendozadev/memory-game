545b7d2b08a758e62a616d7db7e9998f694ce107
Martin Mendoza <martinMendozaDev@gmail.com>
2022-12-09 23:42:13 -0600

    Feat: onFlipped styles card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,30 +1,29 @@
-import { useState } from 'react';
-import { Box, Image } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 
-import { COVER_IMAGE } from '@utils/constants';
 import CardCover from '@components/CardCover';
 
-function Card({ url, name }) {
-  const [imageSRC, setImageSRC] = useState(url);
-
-  const onHandleClick = () => {
-    setImageSRC((prevImage) => (prevImage === url ? COVER_IMAGE : url));
-  };
+function Card({ url, name, flipped }) {
+  const onHandleClick = () => {};
 
   return (
-    <Box m={6} h={40} w={32}>
-      {imageSRC === url ? (
-        <Image
-          alt={name}
-          cursor="pointer"
-          objectFit="cover"
-          src={url}
-          onClick={onHandleClick}
-        />
-      ) : (
-        <CardCover onClick={onHandleClick} />
-      )}
-    </Box>
+    <Flex
+      align="center"
+      cursor="pointer"
+      justify="center"
+      h={40}
+      m={6}
+      onClick={onHandleClick}
+      w={32}
+      transform={flipped && 'rotateY(180deg)'}>
+      <Image
+        alt={name}
+        objectFit="cover"
+        src={url}
+        transform={!flipped && 'rotateY(180deg)'}
+        position="absolute"
+      />
+      <CardCover flipped={flipped} />
+    </Flex>
   );
 }
 
